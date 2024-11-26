198ca3890d7a66b5b637523c9ca998cbb1108f21
DaveVanStaden <dave.vanstaden@student.hku.nl>
2024-11-26 16:50:15 +0100

    Pause and menu controlling with controller

--- LuminousRealmGames/Assets/Scripts/Menu/MainMenu.cs
+++ LuminousRealmGames/Assets/Scripts/Menu/MainMenu.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenuFirst;
+    private void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(mainMenuFirst);
+    }
     public void StartGame()
     {
         // Load the first level
---
+++ LuminousRealmGames/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    public bool isPaused = false;
+    [SerializeField] private GameObject pauseMenuFirst;
+    public void PauseGame()
+    {
+        EventSystem.current.SetSelectedGameObject(pauseMenuFirst);
+        isPaused = true;
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+    }
+    public void UnpauseGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+    }
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            UnpauseGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void OpenSettings()
+    {
+
+    }
+}
