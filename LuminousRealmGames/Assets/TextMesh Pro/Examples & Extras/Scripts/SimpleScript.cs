73153935a69398ee148fa685ab72ab7be7d91389
LiselotteBeijen <liselottebeijen@gmail.com>
2024-12-03 10:42:48 +0100

    END DAY
    Ui dingen geprobeerd. push deel 1

---
+++ LuminousRealmGames/Assets/TextMesh Pro/Examples & Extras/Scripts/SimpleScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GlowOnMouseClick : MonoBehaviour
+{
+    private TextMeshProUGUI textComponent;
+    private Material textMaterial;
+
+    void Start()
+    {
+        textComponent = GetComponent<TextMeshProUGUI>();
+        if (textComponent != null)
+        {
+            textMaterial = textComponent.fontMaterial;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Linker muisknop
+        {
+            ToggleGlow(true);
+        }
+
+        if (Input.GetMouseButtonUp(0)) // Loslaten muisknop
+        {
+            ToggleGlow(false);
+        }
+    }
+
+    void ToggleGlow(bool enable)
+    {
+        if (textMaterial != null)
+        {
+            textMaterial.SetFloat(ShaderUtilities.ID_GlowPower, enable ? 1.0f : 0.0f);
+        }
+    }
+}
